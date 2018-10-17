82979cc701c0b91991b9a8be7579603d8684133a
zry754331875 <754331875@qq.com>
2018-10-17 15:39:20 +0800

    Initial commit

---
+++ src/collectionView.jsx
@@ -0,0 +1,23 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+
+
+class CollectionView extends PureComponent {
+
+  render() {
+    return (
+      <div>
+        欢迎使用CollectionView
+      </div>
+    )
+  }
+}
+
+CollectionView.propTypes = {
+  
+}
+CollectionView.defaultProps = {
+  
+}
+
+export default CollectionView;
