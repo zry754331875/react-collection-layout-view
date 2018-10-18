ea05bf5aab5aa8915e52a8fc849a5ab0c4bbdf94
zry754331875 <754331875@qq.com>
2018-10-18 16:31:47 +0800

    1

--- src/collectionView.jsx
+++ src/collectionView.jsx
@@ -5,6 +5,11 @@ import PropTypes from 'prop-types'
 class CollectionView extends PureComponent {
 
   render() {
+
+    if(this.props.children instanceof Array){
+
+    }
+    
     return (
       <div>
         欢迎使用CollectionView
